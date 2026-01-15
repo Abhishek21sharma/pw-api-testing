ec2d8f954e4d30423437b041a604ca7557c8a74e
Abhishek Sharma <abhi21bhardwaj@gmail.com>
2026-01-15 17:46:14 +0000

    updated helper.. will be keep updating it for ref

--- reUsableMethodsConceptsFromNitroProject/generic-helper.ts
+++ reUsableMethodsConceptsFromNitroProject/generic-helper.ts
@@ -16,3 +16,18 @@ export function getToken(response: any) {
   //IMPORTANT LOGIC HERE
   return response.body.access_token || "";
 }
+
+//IMPORTANT see below helper method..
+//see no param expected but explictily defined the 'return type'
+const generateRandomInt = (): string => {
+  const timeStamp = Date.now().toString();
+  const random = Math.floor(Math.random() * 1e6)
+    .toString()
+    .padStart(6, "0");
+  return `${timeStamp}${random}`;
+};
+
+//IMPORTANT..
+const generateByPAttern = (pattern: string): string => {
+  return "randomPAtterns..";
+};
